a44845a995ac1e656a37880d9dc0aac4a3c53628
azrulwm <azrulazmi.wm@gmail.com>
2022-11-03 11:54:24 +0800

    creating classes folder and incorporating the use of modules

--- public/app.js
+++ public/app.js
@@ -1,15 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
@@ -17,7 +6,7 @@ invoices.push(invOne);
 invoices.push(invTwo);
 console.log(invoices);
 invoices.forEach((inv) => {
-    console.log(inv.client, inv.details, inv.amount, inv.format());
+    console.log(inv.client, inv.amount, inv.format());
 });
 const form = document.querySelector(".new-item-form");
 //inputs
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,4 @@
-// classes
-class Invoice {
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
@@ -22,7 +11,7 @@ invoices.push(invTwo);
 console.log(invoices);
 
 invoices.forEach((inv) => {
-  console.log(inv.client, inv.details, inv.amount, inv.format());
+  console.log(inv.client, inv.amount, inv.format());
 });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
