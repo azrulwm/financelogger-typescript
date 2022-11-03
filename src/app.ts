59e1bb73080f895b578128cfc51e5c7a69e9967c
azrulwm <azrulazmi.wm@gmail.com>
2022-11-03 11:33:58 +0800

    creating classes template for DOM input

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
-// const anchor = document.querySelector("a")!;
+// classes
+class Invoice {
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
-// console.log(anchor.href);
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
 
-// const form = document.querySelector('form')!;
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.details, inv.amount, inv.format());
+});
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-
-// console.log(form.children);
 
 //inputs
 const type = document.querySelector("#type") as HTMLSelectElement;
